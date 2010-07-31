db88eab4fea764e8c10aa1de658930ae3b6c43d9
Marc Weber <marco-oweber@gmx.de>
2010-07-31 05:22:07 +0200

    style: fix indentation

--- tests/lang/TestBitwise.hx
+++ tests/lang/TestBitwise.hx
@@ -4,7 +4,7 @@ import utest.Assert;
 
 class TestBitwise {
 	public function new() {}
-	
+
 	public function testSHL() {
 		Assert.equals(320, 5<<6);
 	}
@@ -29,16 +29,16 @@ class TestBitwise {
 #end
 		Assert.equals(-512, -1024 >> 1);
 	}
-	
+
 	public function testRightShift() {
 		Assert.equals(15, -4 >>> 28);
-		
+
 		var expected = [
 			{ age : 20, gender : 0, height : 5 },
 			{ age : 19, gender : 1, height : 12 },
 			{ age : 4, gender : 0, height : 1 },
 		];
-		
+
 		for (e in expected)
 		{
 			var packed_info = (((e.age << 1) | e.gender) << 7) | e.height;
@@ -50,7 +50,7 @@ class TestBitwise {
 			Assert.equals(e.height, height);
 		}
 	}
-	
+
 
 	public function testXOR() {
 		Assert.equals(481923, 25968 ^ 475123);
@@ -70,73 +70,73 @@ class TestBitwise {
 		Assert.equals(0xFF52, 0xFF00 | 0x52);
 	}
 
-  public function testSHLAssign() {
-    var x = 5;
-    x <<= 6;
+	public function testSHLAssign() {
+		var x = 5;
+		x <<= 6;
 		Assert.equals(320, x);
 	}
 
 	public function testSHRAssign() {
-    var x = 320;
-    x >>= 6;
+		var x = 320;
+		x >>= 6;
 		Assert.equals(5, x);
 
-    x = 320;
-    x >>>= 6;
+		x = 320;
+		x >>>= 6;
 
 		Assert.equals(5, x);
 	}
 
 	public function testNegSHLAssign() {
-    var x = -512;
-    x <<= 2;
+		var x = -512;
+		x <<= 2;
 		Assert.equals(-2048, x);
 	}
 
 #if known_to_fail
 	public function testNegSHRAssign() {
-    var x = -1024;
-    x >>>= 1;
+		var x = -1024;
+		x >>>= 1;
 		Assert.equals(2147483136, x);
-    x = -1024;
-    x >>= 1;
+		x = -1024;
+		x >>= 1;
 		Assert.equals(-512, x);
 	}
 #end
 
 	public function testXORAssign() {
-    var x = 475123;
-    x ^= 25968;
+		var x = 475123;
+		x ^= 25968;
 		Assert.equals(481923, x);
-    x = 341;
-    x ^= 682;
+		x = 341;
+		x ^= 682;
 		Assert.equals(1023, x);
 	}
 
 	public function testANDAssign() {
-    var x = 1024;
-    x &= 1024;
+		var x = 1024;
+		x &= 1024;
 		Assert.equals(1024, x);
-    x = 0xFF52;
-    x &= 0xFF;
+		x = 0xFF52;
+		x &= 0xFF;
 		Assert.equals(0x52, x);
-    x = 0xFF52;
-    x &= 0xFF00;
+		x = 0xFF52;
+		x &= 0xFF00;
 		Assert.equals(0xFF00, x);
 	}
 
 	public function testORAssign() {
-    var x = 1024;
-    x |= 1024;
+		var x = 1024;
+		x |= 1024;
 		Assert.equals(1024, x);
-    x = 0xFF52;
-    x |= 0xFF;
+		x = 0xFF52;
+		x |= 0xFF;
 		Assert.equals(0xFFFF, x);
-    x = 0xFF52;
-    x |= 0xFF00;
+		x = 0xFF52;
+		x |= 0xFF00;
 		Assert.equals(0xFF52, x);
-    x = 0xFF00;
-    x |= 0x52;
+		x = 0xFF00;
+		x |= 0x52;
 		Assert.equals(0xFF52, x);
 	}
 }

099251396d95b43bf7a31a2c5eb6822ab25a651e
Marc Weber <marco-oweber@gmx.de>
2010-07-31 14:11:44 +0200

    make TestIssue61 compile to JS

--- tests/issue/TestIssue61.hx
+++ tests/issue/TestIssue61.hx
@@ -16,5 +16,9 @@ class TestIssue61
 		Assert.equals(2.0, abs( -2.0));
 	}
 	
+#if js
+	public static inline function abs(a:Float) {return a < 0 ? -a : a;}
+#else
 	public static inline function abs(a:Float) (return a < 0 ? -a : a)
+#end
 }
